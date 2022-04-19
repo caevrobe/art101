4bc0176932417ff23233907a3ead681e3a9101d2
swag man <caevrobe@ucsc.edu>
2022-04-18 22:56:42 -0700

    Finishing lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,9 @@
+// Lab 4 computer task pseudocode
+//   This script contains pseudocode for organizing log files
+//   Author: Cameron Robertson
+
+//  Moving program log files into directories based on their date
+//  For each log file in your main log directory:
+//    Extract the month and year from the filename (dd-mm-yyyy)
+//    If folders don’t exist for year/month combo, create them
+//    Move file to its respective directory
