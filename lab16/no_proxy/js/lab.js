aa60e2ebc4a583160c88652502312f04863dec2f
swag man <caevrobe@ucsc.edu>
2022-05-31 08:19:17 -0700

    Added new version of lab 16 which uses original XKCD API instead of proxy

---
+++ lab16/no_proxy/js/lab.js
@@ -0,0 +1,71 @@
+/*
+ * Author:  Cameron Robertson
+ * Created: 5/30/22
+ */
+
+const URL = 'https://xkcd.com/'; // XKCD proxy URL
+var latest_num = 0; // indicates most recent XKCD comic number
+
+
+// loads the previous or next comic (depending on the sign of dir)
+function advance(dir) {
+   let which = Number($('#num').val())+1*Math.sign(dir);
+   if (!(1 <= which && which <= latest_num))
+      return;
+
+   getComic(which);
+   $('#num').val(which);
+}
+
+// constrain number input to [1, latest comic #]
+$('#num').on('input', (e) => {
+   let v = $('#num').val();
+
+   if (v > latest_num)
+      $('#num').val(latest_num);
+   else if (v < 1 && v != '')
+      $('#num').val(1);
+});
+
+// fetch comic when number entry loses focus
+$('#num').focusout((e) => {
+   let v = $('#num').val();
+   if (v != '')
+      getComic(v);
+});
+
+// trigger focusout on number entry when Enter is pressed
+$('#num').keypress((e) => {
+   if (e.key === 'Enter')
+      $('#num').trigger('blur');
+});
+
+
+// fetch and display comic
+function getComic(num='latest', first_time=false) {
+
+   console.log(first_time ? `${URL}info.0.json` : `${URL}${num}/info.0.json`)
+   $.ajax({
+      url: first_time ? `${URL}info.0.json` : `${URL}${num}/info.0.json`,
+      type: 'GET',
+      dataType: 'JSON',
+      success: (data) => {
+         if (first_time)
+            $('#num').val(data.num);
+
+         if (num === 'latest') {
+            $('#num').attr({'max': data.num});
+            latest_num = data.num;
+         }
+
+         $('#comic').attr('src', data.img);
+         $('#comic').attr('alt', data.alt);
+         $('#comic').attr('title', data.alt);
+      },
+      error: (jqXHR, status, err) => {
+         console.warn(jqXHR.responseText);
+      }
+   });
+}
+
+getComic(num='latest', first_time=true);
